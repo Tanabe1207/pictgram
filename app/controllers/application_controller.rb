be197d8cc3f7c05f2c7cbc04fb77fd381beb37d0
root <root@DESKTOP-FJJP9AR.localdomain>
2018-03-03 15:22:22 +0900

    add comment to first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,8 @@ class ApplicationController < ActionController::Base
     # モデル名.find_byは特定の条件にマッチした最初のレコードを取得の意味https://www.sejuku.net/blog/13000参照。
     #ここではsessionで入力されたUserのテーブルのを取得し@current_userに代入している。
 
+    #github test
+
   end
 
   def logged_in?
